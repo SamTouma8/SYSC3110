0f48c22bce9616250b3fcdc4c033ed86e4d0922e
samtouma <samtouma@cmail.carleton.ca>
2024-09-19 21:02:33 -0400

    Version 2

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -25,7 +25,10 @@ public class AddressBook {
 
     // Main method to demonstrate the functionality
     public static void main(String[] args) {
-        System.out.println("Address book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
 
     }
 }
