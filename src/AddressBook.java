b69090eae07a503f8acc5660370e8176575ce75f
samtouma <samtouma@cmail.carleton.ca>
2024-09-22 22:16:46 -0400

    Version 3

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -29,7 +29,7 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
-        System.out.println("test version2");
+        System.out.println("test version3");
 
     }
 }
